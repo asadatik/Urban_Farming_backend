704a42fa1d4f2f212a10e2dd1a6b6462d8ea00da
Asad Atik <asadatik1995@gmail.com>
2026-04-22 00:35:53 +0600

    finalize centralized routing map

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 import authRoutes from '../modules/auth/auth.routes';
 import produceRoutes from '../modules/produce/produce.routes';
+import vendorRoutes from '../modules/vendor/vendor.routes';
+import trackingRoutes from '../modules/tracking/tracking.routes';
+import userRoutes from '../modules/user/user.routes';
+import rentalRoutes from '../modules/rental/rental.routes';
+import orderRoutes from '../modules/order/order.routes';
+import communityRoutes from '../modules/community/community.routes';
+import certificationsRoutes from '../modules/certification/certification.routes';
 
 const router = express.Router();
 
@@ -10,12 +17,42 @@ const moduleRoutes = [
     route: authRoutes,
   },
   {
+    path:'/users',
+    route:userRoutes,
+  },
+  {
     path: '/produce',
     route: produceRoutes,
-
+  },
+  {
+  path: '/vendors',
+  route:vendorRoutes,
+  },
+  {
+    path: '/tracking',
+    route: trackingRoutes,
+  },
+  {
+    path:'/rentals',
+    route:rentalRoutes,
+  },
+  {
+    path: '/orders',
+    route: orderRoutes,
+  },
+  {
+    path: '/community',
+    route: communityRoutes,
+  },
+  {
+    path: '/certifications',
+    route: certificationsRoutes,
   }
+
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
+
+
